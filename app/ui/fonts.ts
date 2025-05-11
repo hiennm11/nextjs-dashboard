a465ef351a8784efcca4b84ef00e8fc03e9a3036
Guanghong Xie <hien.tran@fractal.vn>
2025-05-11 18:08:28 +0700

    update to chapter 6

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Playwrite_DK_Loopet, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const playwright = Playwrite_DK_Loopet();
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
