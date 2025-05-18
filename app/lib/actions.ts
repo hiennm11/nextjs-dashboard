f9433d0d4530b68c222af7896ac08bff9cd4fbc9
Guanghong Xie <hien.tran@fractal.vn>
2025-05-19 00:06:55 +0700

    fix to build chapter 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -61,7 +61,8 @@ export async function createInvoice(prevState: State, formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
-    // If a database error occurs, return a more specific error.
+    // If a database error occurs, return a more specific error./
+    console.error(error);
     return {
       message: 'Database Error: Failed to Create Invoice.',
     };
